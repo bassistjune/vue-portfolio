7ae0d41284ae9fc47eaf1b40f0e7abc634dd7a6f
bassistjune <bassistjune@gmail.com>
2023-06-20 00:17:55 +0900

    vue.js_docs update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath: '/project-myhome/',
+  publicPath: '/project-myhome/',
+  outputDir: 'docs',
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
