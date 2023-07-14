8c976317d4b19fbfd6153535bf4f85edc1f9d983
bassistjune <bassistjune@gmail.com>
2023-07-14 20:35:16 +0900

    #__배포를 위한 vue.config.js 재설정

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
-  publicPath: '/<repository name>/',
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: '',
