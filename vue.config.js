78eec60aec03ac0b4274f412fe7c771707a73253
bassistjune <bassistjune@gmail.com>
2023-07-19 21:02:29 +0900

    #__vue.js_vue.config.js수정

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
   lintOnSave: false,
-  publicPath: '/',
+  publicPath: '/vue-portfolio',
   devServer: {
     allowedHosts: 'all',
     historyApiFallback: true
