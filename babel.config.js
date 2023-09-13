3ee77b96316afbe7db8ceb29a9312e6eb686f82b
bassistjune <bassistjune@gmail.com>
2023-09-13 12:37:42 +0900

    라이브러리설정변경

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+const removeConsolePlugin = []
+if(process.env.NODE_ENV === 'production') {
+  removeConsolePlugin.push([
+    'transform-remove-console',
+    { exclude: ['error', 'warn'] },
+  ])
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins: removeConsolePlugin
 }
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  outputDir: './docs',
+  outputDir: 'docs',
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/vue-portfolio/',
   devServer: {
