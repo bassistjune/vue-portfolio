e6b4b8409f08969b08f7f759d0ec351a76796afd
bassistjune <bassistjune@gmail.com>
2023-09-13 14:38:04 +0900

    jsConfig__baseUrl 충돌

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: 'docs',
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/',
   devServer: {
     allowedHosts: 'all',
     historyApiFallback: true
