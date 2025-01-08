22e040d28b594aac4a84bebf5604bfd75f9fda0e
bassistjune <75768774+bassistjune@users.noreply.github.com>
2025-01-08 15:19:57 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/',
+  publicPath: '/',
   devServer: {
     allowedHosts: 'all',
     historyApiFallback: true
