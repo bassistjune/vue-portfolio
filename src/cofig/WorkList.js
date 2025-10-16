3d999695bb75ef3fb8e213a32525ad3fc72a41e3
bassistjune <bassistjune@gmail.com>
2025-10-16 09:58:10 +0900

    포트폴리오 페이지 수정

--- src/cofig/WorkList.js
+++ src/cofig/WorkList.js
@@ -6,15 +6,15 @@ const works = [
     },
     back: {
       bTit: '영품문고 리뉴얼 (쇼핑몰, 관리자 페이지)',
-      bLists:[
+      bLists: [
         {
           bListTit: '관련 사용기술',
-          bListDesc: [ 'Vue.js3', 'Emitter', 'Moment', 'lodash', 'swiper', 'v-click-outside' , 'vee-validate', 'tui-date-picker', 'dompurify', 'vue3-print-nb'],
+          bListDesc: ['Vue.js3', 'Emitter', 'Moment', 'lodash', 'swiper', 'v-click-outside', 'vee-validate', 'tui-date-picker', 'dompurify', 'vue3-print-nb']
         },
         {
           bListTit: '작업내역 - 영푼문고 리뉴얼 사이트',
           bListDesc: [
-            '이슈내용: ',
+            '이슈내용: ' +
             '검색인풋 포커스시 키,마우스 이벤트(호버 및 엔터 또는 클릭 시 검색어 변경등)',
             '테마카테고리 선택시 추천 테마 페이지 변경', '장바구니 재접속시 이전 데이터 초기화X(ex. 책 갯수 등)',
             '검색 리스트 데이터 변동시 기능 초기화(페이지네이션등)',
@@ -26,17 +26,15 @@ const works = [
             '성인인증 및 각종 메시지 기능 추가 ',
             '각종 CSS 문제 수정 - 주로 Flex 관련 또는 text-overflow, max-width, float(clear)등의 문제',
             '모달 팝업 두번 여닫을 시 스크롤 고정 - 라이브러리 내 바닐라 스크립트(css) 문제 수정등'
-          ],
+          ]
         },
         {
           bListTit: '작업내역 - 관리자 페이지 리뉴얼',
-          bListDesc: ['성인인증 요청 및 Pass인증으로 이동', '검색리스트 목록 Api변경시 '],
+          bListDesc: ['성인인증 요청 및 Pass인증으로 이동', '검색리스트 목록 Api변경시 ']
         }
-      ]
-      ,
-      btn: '오픈예정 상태, 이동 불가',
-      btnUrl: 'https://www.skylife.co.kr/BE/event/detail?id=652',
-      btnClass: 'disable'
+      ],
+      btn: '영푼문고 바로가기',
+      btnUrl: 'https://www.ypbooks.co.kr/'
     }
   },
   {
@@ -46,11 +44,11 @@ const works = [
     },
     back: {
       bTit: '이벤트 페이지 공유하기',
-      bLists:[
+      bLists: [
         {
           bListTit: '관련 사용기술',
-          bListDesc: [ 'Nuxt.Js', ' Fb.sdk', ' Kakao\.sdk', ' ClipBoard Api', ' AsyncData', ' mixins', ' ngrok' ]
-        },
+          bListDesc: ['Nuxt.Js', ' Fb.sdk', ' Kakao', ' ClipBoard Api', ' AsyncData', ' mixins', ' ngrok']
+        }
       ],
       btn: 'Url로 이동',
       btnUrl: 'https://www.skylife.co.kr/BE/event/detail?id=652'
@@ -63,10 +61,10 @@ const works = [
     },
     back: {
       bTit: '유심가입 페이지 (이용시간)',
-      bLists:[
+      bLists: [
         {
           bListTit: '관련 사용기술',
-          bListDesc: ['Nuxt.Js', ' JS(Es6)', ' RegExp', ' Rx.Js', ' Vue-Router', ' Watch', ' V-Model'],
+          bListDesc: ['Nuxt.Js', ' JS(Es6)', ' RegExp', ' Rx.Js', ' Vue-Router', ' Watch', ' V-Model']
         }
       ],
       btn: 'Url로 이동',
@@ -80,7 +78,7 @@ const works = [
     },
     back: {
       bTit: '모바일 로그인 - 번호,생년월일,sms인증',
-      bLists:[
+      bLists: [
         {
           bListTit: '관련 사용기술',
           bListDesc: ['Nuxt.Js', ' JS(Es6)', ' Moment.Js', ' Rx.Js']
@@ -97,11 +95,11 @@ const works = [
     },
     back: {
       bTit: '신규 페이지 및 상품 생성',
-      bLists:[
+      bLists: [
         {
           bListTit: '관련 사용기술',
           bListDesc: ['Nuxt.Js', ' JS(Es6)', ' i18n', ' Rx.Js', ' ui관련 라이브러리(셀렉터등)']
-        },
+        }
       ],
       btn: 'Url로 이동',
       btnUrl: 'https://www.skylife.co.kr/product/tv/service/app/disneyplus',
@@ -115,10 +113,10 @@ const works = [
     },
     back: {
       bTit: 'KS-SQI(반응형 페이지 제작)',
-      bLists:[
+      bLists: [
         {
           bListTit: '관련 사용기술',
-          bListDesc: ['Html', ' JS(Es6)', ' J-Query', ' Aos', ' Gsap', ' ScrollTrigger', ' 풀페이지와 슬라이더&Progress등을 새로 제작'],
+          bListDesc: ['Html', ' JS(Es6)', ' J-Query', ' Aos', ' Gsap', ' ScrollTrigger', ' 풀페이지와 슬라이더&Progress등을 새로 제작']
         }
       ],
       btn: 'Url로 이동',
