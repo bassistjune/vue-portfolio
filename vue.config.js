26bba58b58308126c25fae2d16baf4d0630c753c
bassistjune <bassistjune@gmail.com>
2025-10-16 10:06:14 +0900

    포트폴리오 페이지 수정

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-portfolio/' : '/',
+  publicPath: '/',
   devServer: {
     allowedHosts: 'all',
     historyApiFallback: true
